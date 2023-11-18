83fa85cbabf8eaf31798c96e2e552beb601202b2
Dylankm86 <335478@student.mboutrecht.nl>
2023-11-18 15:03:21 +0100

    Display of the hourly forcasr function added to thejs

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 document.getElementById('searchBtn').addEventListener('click', function() {
     const city = document.getElementById('cityInput').value;
     const apiKey = 'bd01bc2635c934b37257f0e5cb3c9572';
+
+    fetchCurrentWeather(city, apiKey);
+    fetchForecast(city, apiKey);
+});
+
+function fetchCurrentWeather(city, apiKey) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
+        .then(response => response.json())
+        .then(data => updateWeatherDashboard(data))
+        .catch(error => console.error('Error:', error));
+}
+
+function fetchForecast(city, apiKey) {
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(forecastUrl)
+        .then(response => response.json())
         .then(data => {
-            updateWeatherDashboard(data);
+            displayHourlyForecast(data);
+            displayDailyForecast(data);
         })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-});
+        .catch(error => console.error('Error:', error));
+}
 
 function updateWeatherDashboard(data) {
     document.getElementById('temperature').textContent = `Temperature: ${data.main.temp}°C (Feels like: ${data.main.feels_like}°C)`;
@@ -28,4 +37,22 @@ function updateWeatherDashboard(data) {
     document.getElementById('pressure').textContent = `Pressure: ${data.main.pressure} hPa`;
     document.getElementById('visibility').textContent = `Visibility: ${data.visibility / 1000} km`;
     document.getElementById('cloudiness').textContent = `Cloudiness: ${data.clouds.all}%`;
+}
+
+function displayHourlyForecast(data) {
+    const hourlyForecastDiv = document.getElementById('hourlyForecast');
+    hourlyForecastDiv.innerHTML = '';
+
+    for (let i = 0; i < 5; i++) {
+        const forecast = data.list[i];
+        const forecastDiv = document.createElement('div');
+        forecastDiv.classList.add('forecast-item');
+        forecastDiv.innerHTML = `
+            <h4>${new Date(forecast.dt_txt).toLocaleTimeString()}</h4>
+            <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png" alt="Weather Icon">
+            <p>Temp: ${forecast.main.temp}°C</p>
+            <p>${forecast.weather[0].description}</p>
+        `;
+        hourlyForecastDiv.appendChild(forecastDiv);
+    }
 }
